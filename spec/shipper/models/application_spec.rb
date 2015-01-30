9a3d995600d47e717117a331dd1fc648cb38f20c
Hawk Newton <hawk.newton@gmail.com>
2015-01-29 16:12:53 -0800

    Disallow duplicate container names

--- spec/shipper/models/application_spec.rb
+++ spec/shipper/models/application_spec.rb
@@ -6,10 +6,10 @@ describe Application do
   it { is_expected.to respond_to :containers= }
 
   describe '::read' do
-    subject { described_class.read file }
+    subject { described_class.read test_file file }
 
     context 'given a simple file' do
-      let(:file) { test_file 'single.json' }
+      let(:file) { 'single.json' }
 
       its('containers.size') { is_expected.to eq 1 }
 
@@ -20,6 +20,12 @@ describe Application do
       it 'should set the container tag' do
         expect(subject.containers[0].tag).to eq 'hawknewton/show-env'
       end
+    end
+
+    context 'given two containers with the same name' do
+      let(:file) { 'duplicate.json' }
+
+      example { expect { subject }.to raise_error(/have the same name: duplicate/) }
     end
   end
 end
