083da70cda0d0524aa54e5ada7d717a2de691e04
Hawk Newton <hawk.newton@gmail.com>
2015-02-11 06:21:03 -0800

    dev action

--- lib/shipper/actions/dev_action.rb
+++ lib/shipper/actions/dev_action.rb
@@ -1,2 +1,13 @@
 class DevAction
+  def initialize(file, profiles = [])
+    @file = file
+    profiles.include?('dev') || profiles << 'dev'
+    @profiles = profiles
+  end
+
+  def perform
+    RunAction.new(@file, @profiles).perform
+    [:SIGTERM, :SIGINT].each { |s| Signal.trap(s) { DestroyAction.new(@file).perform } }
+    LogAction.new(@file).perform
+  end
 end
