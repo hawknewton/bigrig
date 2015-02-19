fd9a266d1eb4e52abadbcc7557e8caf8114b56ab
Hawk Newton <hawk.newton@gmail.com>
2015-02-19 09:08:52 -0800

    Don't freak out when profiles are missing

--- lib/shipper/descriptor.rb
+++ lib/shipper/descriptor.rb
@@ -15,11 +15,7 @@ module Shipper
 
     def self.read(file, active_profiles = [])
       json = JSON.parse File.read file
-      profiles = active_profiles.map do |profile|
-        [profile, json['profiles'][profile]]
-      end
-      profile_map = Hash[*profiles.flatten].select { |_n, v| !v.nil? }
-      Descriptor.new json['containers'], profile_map
+      Descriptor.new json['containers'], profiles_for(json, active_profiles)
     end
 
     private
@@ -32,6 +28,15 @@ module Shipper
           container_map[container][key] = value
         end
       end
+    end
+
+    def self.profiles_for(json, active_profiles)
+      if json['profiles']
+        profiles = active_profiles.map do |profile|
+          [profile, json['profiles'][profile]]
+        end
+      end
+      Hash[*(profiles || []).flatten].select { |_n, v| !v.nil? }
     end
   end
 end
