0be8bf69c5a01e933fbe7b27a9073447946702f6
Hawk Newton <hawk.newton@gmail.com>
2015-02-20 11:05:03 -0800

    bring down method size & complexity

--- lib/shipper/actions/log_action.rb
+++ lib/shipper/actions/log_action.rb
@@ -16,14 +16,24 @@ module Shipper
       threads = @application.containers.map do |container|
         color = next_color
         Thread.new do
-          DockerAdapter.logs(container.name) do |s, c|
-            puts "#{container.name.send color}: #{s == :stderr ? c.light_red : c}"
-            $stdout.flush
-          end
+          DockerAdapter.logs container.name, &print_block(container.name.send color)
         end
       end
 
       threads.each(&:join)
+    end
+
+    private
+
+    def print_block(name)
+      proc do |stream, chunk|
+        if stream == :stderr
+          print "#{name}: #{chunk.light_red}"
+        else
+          puts "#{name}: #{chunk}"
+        end
+        $stdout.flush
+      end
     end
 
     def next_color
--- spec/shipper/actions/log_action_spec.rb
+++ spec/shipper/actions/log_action_spec.rb
@@ -1,7 +1,8 @@
 module Shipper
   describe LogAction do
     describe '#perform' do
-      subject { described_class.new(file).perform }
+      subject { runner.perform }
+      let(:runner) { described_class.new(file) }
       let(:file) { test_file 'log.json' }
       let(:image) { Docker::Image.create 'fromImage' => 'hawknewton/log-test:0.0.1' }
       let(:container) { Docker::Container.get 'log-test' }
@@ -21,7 +22,7 @@ module Shipper
           and_yield(:stdout, 'stdout message').
           and_yield(:stderr, 'stderr message')
         expect($stdout).to receive(:puts).with "\e[0;32;49mlog-test\e[0m: stdout message"
-        expect($stdout).to receive(:puts).
+        expect(runner).to receive(:print).
           with "\e[0;32;49mlog-test\e[0m: \e[0;91;49mstderr message\e[0m"
         subject
       end
