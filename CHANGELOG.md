8cd1377278520af252d2b620cdea512e34f31333
Hawk Newton <hawk.newton@gmail.com>
2015-03-05 12:34:10 -0800

    Added authentication for push

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,8 @@
-# 0.0.2
+0.0.2
+=====
 * Profiles can add new containers
+
+0.0.3
+=====
+* `bigrig ship` takes credentials
+
--- spec/bigrig/actions/ship_action_spec.rb
+++ spec/bigrig/actions/ship_action_spec.rb
@@ -1,6 +1,6 @@
 module Bigrig
   describe ShipAction do
-    subject { described_class.new(file_name, version).perform }
+    subject { described_class.new(file_name, version, credentials).perform }
     let(:dir) { Dir.mktmpdir }
     let(:version) { '1.2.3' }
     let(:file) { 'ship.json' }
@@ -26,6 +26,7 @@ module Bigrig
       let(:path) { json['containers']['ship-me']['path'] }
       let(:file_name) { 'bigrig.json' }
       let(:exists?) { File.exist? File.join(dir, "bigrig-#{version}.json") }
+      let(:credentials) { nil }
 
       it 'creates a bigrig.json with a version' do
         subject
@@ -45,12 +46,36 @@ module Bigrig
 
     context 'with a different file name' do
       let(:opts) { {} }
+      let(:credentials) { nil }
       let(:file_name) { 'custom.json' }
       let(:exists?) { File.exist? File.join(dir, "custom-#{version}.json") }
 
       it 'creates a file with that name' do
         subject
         expect(exists?).to be true
+      end
+    end
+
+    context 'with credentials' do
+      let(:opts) { {} }
+      let(:credentials) do
+        {
+          username: 'user',
+          password: 'pass',
+          email: 'email@email.com'
+        }
+      end
+      let(:file_name) { 'bigrig.json' }
+
+      it 'hands the token to DockerAdapter' do
+        expected = {
+          'username' => 'user',
+          'password' => 'pass',
+          'email' => 'email@email.com',
+          'serveraddress' => 'https://index.docker.io/v1'
+        }
+        expect(DockerAdapter).to receive(:push).with(':1.2.3', expected)
+        subject
       end
     end
   end
