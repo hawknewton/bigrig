15916a034ddcee421c29bf9631f25176e96f8848
Hawk Newton <hawk.newton@gmail.com>
2015-04-06 15:04:42 -0700

    Unbreak TravisCI

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -3,12 +3,17 @@ require 'webmock'
 require 'docker'
 
 VCR.configure do |c|
+  docker_host = URI.parse(Docker.url).host
   c.allow_http_connections_when_no_cassette = false
-  c.filter_sensitive_data('<DOCKER_HOST>') { Docker.url.sub(/tcp\:/, 'https:') }
-  c.filter_sensitive_data('<DOCKER_HTTP>') { "http:#{Docker.url.split(':')[1]}:4567" }
   c.filter_sensitive_data('<USERNAME>') { ENV['DOCKER_API_USER'] }
   c.filter_sensitive_data('<PASSWORD>') { ENV['DOCKER_API_PASS'] }
   c.filter_sensitive_data('<EMAIL>') { ENV['DOCKER_API_EMAIL'] }
+
+  c.define_cassette_placeholder('<ENCODED_REPO_URL>') { "#{docker_host}%3A5000" }
+  c.define_cassette_placeholder('<REPO_URL>') { "#{docker_host}:5000" }
+  c.define_cassette_placeholder('<TEST_URL>') { "http://#{docker_host}:4568" }
+  c.define_cassette_placeholder('<DOCKER_HOST>') { Docker.url.sub(/tcp\:/, 'https:') }
+  c.define_cassette_placeholder('<DOCKER_HTTP>') { "http:#{Docker.url.split(':')[1]}:4567" }
   c.hook_into :excon, :webmock
   c.cassette_library_dir = File.join(File.dirname(__FILE__), '..', 'vcr')
   defined?(RSpec) && c.configure_rspec_metadata!
